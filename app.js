e115d9bc3d0a241d6d7d5e632fc30cbf41ca723c
adcar <123zezima21@gmail.com>
2018-08-06 01:26:14 -0400

    Removed backwards compat, Added filter param

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ app.use(function(req, res, next) {
 	next()
 })
 
-app.get('/:track/:artist/:duration', (req, res) => {
+app.get('/:track/:artist/:duration/:filter', (req, res) => {
+	// Filter can be anything you want appended to YT search query, but is designed to be either "clean" or "explicit"
 	youtubeSearch('AIzaSyD_uZJQ7E74CoN5D48t8mldAKGUPx9XQ9Y', {
 		q: `${req.params.track.replace(
 			/[^\w\s]/gi,
 			''
-		)} ${req.params.artist.replace(/[^\w\s]/gi, '')} audio`
+		)} ${req.params.artist.replace(/[^\w\s]/gi, '')} audio ${req.params.filter}`
 	})
 		.then(results => {
 			const setUrl = url => {
@@ -54,38 +55,6 @@ app.get('/:track/:artist/:duration', (req, res) => {
 					}
 				)
 			}
-		})
-		.catch(err => console.log(`Youtube search failed: ${err}`))
-})
-
-// Backwards compat
-
-app.get('/:track/:artist', (req, res) => {
-	youtubeSearch('AIzaSyD_uZJQ7E74CoN5D48t8mldAKGUPx9XQ9Y', {
-		q: `${req.params.track.replace(
-			/[^\w\s]/gi,
-			''
-		)} ${req.params.artist.replace(/[^\w\s]/gi, '')} audio`
-	})
-		.then(results => {
-			let videoId = results.items[0].id.videoId
-
-			ytdl.getInfo(
-				`https://www.youtube.com/watch?v=${videoId}`,
-				(err, info) => {
-					console.log('ok')
-					console.log(info)
-					let format = info.formats.find(item =>
-						item.type.includes('audio')
-					)
-					if (format) {
-						res.send(format.url)
-					} else {
-						console.log(`Youtube getInfo failed: ${err}`)
-						res.send('Song not found')
-					}
-				}
-			)
 		})
 		.catch(err => console.log(`Youtube search failed: ${err}`))
 })
